352509d3f5b0d36116d44d2eefe02ab32fb7ed55
Mercenario098 <lucas.azevedo_@outlook.com.br>
2025-03-20 14:58:57 -0300

    add

---
+++ aula c/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int leia();
+
+int main(){
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    int i, n;
+
+    if(scanf("%d %d", &i, &j)){
+    if(n == 1){
+        return;
+    }
+    else if(n == n%2){
+        printf("\nO valor é %d", n = n/2);
+    }
+    else{
+        printf("\nO valor é %d", n = (3*n)+1);
+
+    }
+    }
+
+
+    return 0;
+}
+
+
