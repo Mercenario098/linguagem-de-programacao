5e04be38d87b1858153db2214c6b67b4d9183698
Mercenario098 <lucas.azevedo_@outlook.com.br>
2025-03-23 15:16:43 -0300

    add final

--- Funcionarios.c
+++ Funcionarios.c
@@ -6,14 +6,13 @@ int main(){
     setlocale(LC_ALL, "Portuguese_Brazil");
     int size = 10;
     int sexo[size], idade[size], qtdfilhos[size], estadoCivil[size];
-    int filho = 0;
+    int filho = 0, menor18 = 0, idoso = 0;
     float salario[size];
-    float salarioMedioPaisIdososSolteiro = 0;
-
+    float salarioTotal = 0, salarioMedioPaisIdososSolteiro = 0, maiorSalario;
 
     for (int i = 0; i < size; i++){
             printf("%d° Funcionario!", i+1);
-            printf("\nQual o Seu salario: ");
+            printf("\nQual o Seu salário: ");
             scanf("%f", &salario[i]);
             printf("Qual a sua idade: ");
             scanf("%d", &idade[i]);
@@ -23,18 +22,39 @@ int main(){
             scanf("%d", &estadoCivil[i]);
             printf("Tem filhos (1)Sim (2)Não: ");
             scanf("%d", &filho);
+
             if(filho == 1){
                 printf("Quantos Filhos: ");
                 scanf("%d", &qtdfilhos[i]);
             }
             else{
             }
-
+            if(sexo[i] == 1 && filho == 2){ //Mostrar o salário da mulher que ganha o maior salário e que não tem filhos
+                if(maiorSalario < salario[i]){
+                    maiorSalario = salario[i];
+                }
+            }
+            else if(idade[i] < 18 && filho == 1 && estadoCivil[i] == 1 && salario[i] > 3000){//A quantidade dos pais adolescentes (idade < 18) que não são solteiros que ganham acima de R$ 3000
+                menor18++;
+            }
+            else if(idade[i] >= 60 && filho == 1 && estadoCivil[i] == 2){ //A média salarial dos pais idosos solteiros
+                idoso++;
+                salarioTotal += salario[i];
+            }
 
             printf("\n\n");
     }
-    printf("A média salarial dos pais idosos solteiro é de R$ %.3f reais",salarioMedioPaisIdososSolteiro);
 
+    if(salarioTotal == 0){
+        salarioMedioPaisIdososSolteiro = 0;
+    }
+    else{
+        salarioMedioPaisIdososSolteiro = salarioTotal / idoso;
+    }
+
+    printf("A média salarial dos pais idosos solteiro é de: R$ %.2f reais", salarioMedioPaisIdososSolteiro);
+    printf("\nO salário da mulher solteira e sem filhos é: %.2f reais", maiorSalario);
+    printf("\nO total de  pais adolescentes menor de idade com salário acima de R$ 3000 é: %d ", menor18);
 
     return 0;
 }
