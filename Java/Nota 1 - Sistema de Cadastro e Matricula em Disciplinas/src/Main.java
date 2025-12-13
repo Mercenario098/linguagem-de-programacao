3b81395bf6c93db67418e7b932cd84d8e58a137e
Mercenario098 <lucas.azevedo_@outlook.com.br>
2025-12-13 14:39:14 -0300

    add java

--- Java/Nota 1 - Sistema de Cadastro e Matricula em Disciplinas/src/Main.java
+++ Java/Nota 1 - Sistema de Cadastro e Matricula em Disciplinas/src/Main.java
@@ -1,8 +1,36 @@
+import java.util.List;
 void main() {
     int opcao = 0;
 
     do{
+        IO.println("\t\t\tSistema de Cadastro\n");
+        IO.println("1 - Realizar Matrícula");
+        IO.println("2 - Listar Alunos");
+        IO.println("3 - Listar Alunos por Disciplina");
+        IO.println("4 - Sair");
+        opcao = Integer.parseInt(IO.readln("Opção> "));
+
+        switch (opcao){
+            case 1 :
+
+                break;
+
+            case 2 :
+
+                break;
+            case 3 :
+
+                break;
+            case 4 :
+
+                break;
+            case 5 :
+
+                break;
+            default:
+                IO.println("Opção invalida!");
+        }
 
     }
-    while (opcao == 5);
+    while (opcao == 4);
 }
--- Java/Nota 1 - Sistema de Cadastro e Matricula em Disciplinas/src/Matriculador.java
+++ Java/Nota 1 - Sistema de Cadastro e Matricula em Disciplinas/src/Matriculador.java
@@ -5,16 +5,29 @@ public class Matriculador {
 
     private List<Disciplina> ofertas;
     private List<Aluno> alunos;
+    private int matriculaAutomatica = 00001;
 
-    public Matriculador(){
-
-    }
-    void cadastrarDisciplina(Disciplina d) { //não deve aceitar valores null nem códigos duplicados;
-
+    public void cadastrarDisciplina(Disciplina d) { //não deve aceitar valores null nem códigos duplicados;
+        if (d == null){
+            return;
+        }
+        for (int i = 0; i < ofertas.size(); i++) {
+            Disciplina x = ofertas.get(i);
+            if (x.getCodigo().equals(d.getCodigo())) {
+                IO.println("Código de Disciplina Duplicado!");
+                return;
+            }
+        }
+        ofertas.add(d);
     }
 
     void cadastrarAluno(Aluno a) { //gera automaticamente a matrícula antes de cadastrar o aluno;
+       if (a == null) {
+         return;
+       }
 
+       a.setMatricula(matriculaAutomatica++);
+       alunos.add(a);
     }
 
     boolean matricular(int matricula, String codigoDisciplina) { //realiza a matrícula e retorna true se for bem-sucedida ou false caso não haja vaga ou o aluno já esteja inscrito
